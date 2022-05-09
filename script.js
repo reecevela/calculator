029dfa963257d00c0084f170a14d7c4773bf3a12
Reece Vela <reecevela@outlook.com>
2022-05-09 17:52:27 -0500

    Activate operator buttons

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
 const clearButton = document.querySelector('.clear');
-const displayLabel = document.querySelector('.display-label');
-const oneButton = document.querySelector('.one');
-const addButton = document.querySelector('.add');
 const equalsButton = document.querySelector('.equals');
+const displayLabel = document.querySelector('.display-label');
 const numericButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operation');
 
 let initialVal = "";
-let newVal = null;
+let newVal = "";
 let operation = null;
 let displayVal = 1000;
 
 numericButtons.forEach(button => {
   button.addEventListener('click', () => {
     let val = button.firstChild.textContent;
-    handleButton(val);
+    val == "." ? handleButton(-1) : handleButton(+val);
+    //pass -1 instead of "." to get past "typeof val" in handler
+    //+val turns it into an int
+  }, false);
+});
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    let val = button.firstChild.textContent;
+    handleButton(val);// val (without the +) passes a string
   }, false);
 });
 
 
 function handleButton(val) {
-  if (val == ".") {
-    //(str.indexOf('.') !== -1) can tell if a secimal is already in the string
-    //fucntioanlity to be added later
-    return;
-  }
 
-  if (!operation) {
-    initialVal = "" + initialVal + val;
-    display(initialVal);
+  console.log(initialVal, operation, newVal);
+
+  switch (typeof val) {
+    case 'number':
+      if (val == -1) {val = '.'}; // convert decimals
+
+      if (!operation) { // check if operation's been set yet
+        if (val == '.' && initialVal.indexOf('.') !== -1) {
+          return; //escape early if there's already a decimal point
+        }
+        initialVal = "" + initialVal + val; //convert to string and display it
+        display(initialVal);
+      } else {
+        if (val == '.' && newVal.indexOf('.') !== -1) {
+          return;
+        }
+        newVal = "" + newVal + val;
+        display(newVal);
+      }
+      break;
+    case 'string':
+      operation = val;
+      break;
+    default:
+      console.log('idk???');
+      break;
   }
 }
 
